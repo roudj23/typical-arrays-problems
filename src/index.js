a2dc10ba000b23b0a5a2935593fa573d90baf023
roudj23 <79924346+roudj23@users.noreply.github.com>
2021-03-15 18:31:31 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+      let minArg = array[0];
+      for (let i = 1;i < array.length;i++) {
+          if (array[i] < minArg) {
+              minArg = array[i];
+          }
+      }
+      return minArg;
+  }
+return 0;
 }
-
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+      let maxArg = array[0];
+      for (let i = 1;i < array.length;i++) {
+          if (array[i] > maxArg) {
+              maxArg = array[i];
+          }
+      }
+      return maxArg;
+  }
+return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+      const avgArg = array.reduce((accumulator, value) => accumulator + value, 0)
+      return avgArg / array.length;
+  }
+return 0;
 }
